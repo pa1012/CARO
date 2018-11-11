dad2cd7bf9849453bdb0348d6a344617ab125715
= <=>
2018-11-12 06:20:49 +0700

    done again!

--- carogame/LoadData.h
+++ carogame/LoadData.h
@@ -6,4 +6,4 @@ void loadTexture(string file, sf::Texture &texture);
 void loadFont(string file, sf::Font &font);
 void loadSound(string file, sf::SoundBuffer sound);
 void loadMusic(string file, sf::Music &music);
-void loadAllAssets(string files[], sf::Texture textures[], int no1);
+//void loadAllAssets(string files[], sf::Texture textures[], int no1);
